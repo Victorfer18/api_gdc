ff165e9820fc00fcb1ead961aa7e49bc1e41d4c8
Victorfer18 <victorfernandomagalhaes@gmail.com>
2022-10-03 09:26:16 -0300

    fix

--- src/config.php
+++ src/config.php
@@ -2,8 +2,8 @@
 
 return [
     "host" => "sql362.main-hosting.eu",
-    "user" => "u925461440_evidaApi",
-    "senha" => "hD0THedGU=",
-    "banco" => "u925461440_evidaApi",
+    "user" => "u925461440_EvidaMedApi",
+    "senha" => 'O>$iBc8P:k5#',
+    "banco" => "u925461440_EvidaMed",
     "token" => "N3uohCJ8kKhTMaNVhIpZU8C1tN7OOBEr7QgFBlVak1I",
 ];
