58bcf3a495f06876af862bf26317c5fd0b74a1e5
Victorfer18 <victorfernandomagalhaes@gmail.com>
2022-10-03 08:59:15 -0300

    fix

--- src/config.php
+++ src/config.php
@@ -7,12 +7,3 @@ return [
     "banco" => "u925461440_evidaApi",
     "token" => "N3uohCJ8kKhTMaNVhIpZU8C1tN7OOBEr7QgFBlVak1I",
 ];
-
-
-// return [
-//     "host" => "10.35.253.45",
-//     "banco" => "gdc",
-//     "user" => 'cmed',
-//     "senha" => 'Cmed$Cmed$',
-//     "token" => "N3uohCJ8kKhTMaNVhIpZU8C1tN7OOBEr7QgFBlVak1I",
-// ];
