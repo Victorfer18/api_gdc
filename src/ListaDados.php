9d22d06b248017016fdf6c8731760c6264e6041d
Victorfer18 <victorfernandomagalhaes@gmail.com>
2022-10-03 09:02:06 -0300

    fix

--- src/ListaDados.php
+++ src/ListaDados.php
@@ -2,10 +2,12 @@
 
 class ListaDados{
     static function get_dados_by_data(){
-        $banco = new Banco();
+        // $banco = new Banco();
         $get_token = $_REQUEST['token'];
         $data = $_REQUEST['data'];
-        $base_token = "N3uohCJ8kKhTMaNVhIpZU8C1tN7OOBEr7QgFBlVak1I";
+        $base_token = require_once __DIR__ . '/config.php';
+        $base_token = $base_token['token'];
+        
         if(empty($get_token) or empty($data)){
             echo json_encode([
                 "next" => false,
--- src/configExemple.php
+++ src/configExemple.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    "host" => "sql362.main-hosting.eu",
-    "banco" => "u925461440_evida",
-    "user" => "u925461440_evida",
-    "senha" => "Qkn/NF@Jr|9",
-    "token" => "N3uohCJ8kKhTMaNVhIpZU8C1tN7OOBEr7QgFBlVak1I",
+    "host" => "",
+    "banco" => "",
+    "user" => "",
+    "senha" => "",
+    "token" => "",
 ];
